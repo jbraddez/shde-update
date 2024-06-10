38d55fbd1207c98aff874101d3be5f025ea455d2
jbraddez <157930657+jbraddez@users.noreply.github.com>
2024-06-10 21:34:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,29 @@ function allSizes(){
         links.style.display = 'block';
         nav.style.display = 'none';
     }
+    
 }
 
 window.addEventListener('resize', allSizes);
 window.addEventListener('load', allSizes);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('nav');
+    if (nav) {
+        document.addEventListener('click', function(event) {
+            if (!nav.contains(event.target)) {
+                const checkbox = document.getElementById('touch');
+                if (checkbox) {
+                    checkbox.checked = false;
+                }
+                const navLabel = document.querySelector('nav label');
+                if (navLabel) {
+                    navLabel.style.backgroundColor = '#333';
+                }
+            }
+        });
+    }
+});
+
